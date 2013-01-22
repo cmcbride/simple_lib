7ae705ce2f45c4b4eea50d2276f98f54b09930df
Cameron McBride <cameron.mcbride@gmail.com>
2013-01-22 10:35:13 -0500

    cleaned up and tested, added simple_array.
    Major clean up of code, reorder, and vetted most.  Code is still beta quality,
    but most functionality seems to work.  Added CONST keyword where possible.
    
    Added simple_array utility as well.
    
    Also dropped check_utils from inclusion, this will be a separate repo.

--- simple_reader.c
+++ simple_reader.c
@@ -1,52 +1,204 @@
-/* The intention is a simple ascii / text file reader interface */
+/* a simple ascii / text file reader interface
+ *
+ * Cameron McBride
+ * cameron.mcbride AT gmail.com
+ * January 2013
+ * */
+#pragma once
+#ifndef SIMPLE_READER_DEFINED
+#define SIMPLE_READER_DEFINED 1
 
-#include "check_fopen.c"
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
 
-struct {
-    char * filename;
-    FILE * fp;
-    size_t line_num;
+#ifndef TRUE
+#define TRUE 1
+#define FALSE 0
+#endif
+
+#include <check_fopen.c>
+#include <check_alloc.c>
+
+typedef struct {
+    FILE *fp;
+    char *filename;
+    char *buf;
     size_t buf_size;
-    size_t col_min;
-    char * buf;
+    size_t buf_len;
+    size_t line_num;
 } simple_reader;
 
-/* constructors */
-simple_reader *
-sr_init(char * filename)
+static inline int
+sr_check_not_null( simple_reader const *const sr )
 {
-    simple_reader sr;
-
-    sr.filename = filename;
-    sr.fp = check_fopen(filename, "r");
-
-    sr.line_num = 0;
-
-    sr.buf_size = 1024;
-    sr.col_min = 0;
-
-    return &sr;
+    if( NULL == sr ) {
+        fprintf( stderr, "Error: simple_reader expected to be NOT NULL!\n" );
+        assert( NULL == sr );
+    }
+    return TRUE;
 }
 
-/* configuration and setting */
-// sr_cols_min( simple_reader * sr, size_t n); // ensure at least n columns in line parsing.  Meaningless if not parsed
-// sr_cols_max( simple_reader * sr, size_t n); // ensure at most n columns.  Error out if more.
-// void sr_buf_size(simple_reader * sr, size_t n);     // buffer initialized to this many values.
+void
+sr_buf_alloc( simple_reader * sr, const size_t maxlen )
+{
+    sr_check_not_null( sr );
+    sr->buf_size = maxlen + 2;  /* one for \0 and one to replace possible "\n" */
+    sr->buf = check_realloc( sr->buf, sr->buf_size, sizeof( char ) );
+}
 
-/* line-by-line reading, minimal allocations */
-char * line;
-sr_readline(sr);    // NULL on EOF
+size_t
+sr_buf_maxlen( simple_reader const *const sr )
+{
+    sr_check_not_null( sr );
+    if( sr->buf_size >= 2 )
+        return sr->buf_size - 2;
+    return 0;
+}
 
-/* so useage of while(inputfile_readline(infile)) { ... } */
+void
+sr_buf_clean( simple_reader * sr )
+{
+    if( NULL != sr ) {
+        CHECK_CLEAN( sr->buf );
+        sr->buf_size = 0;
+    }
+}
 
-char * line
-sr_line(sr);        // output current buffered line
+void
+sr_init( simple_reader * const sr, char const *const filename )
+{
+    size_t len;
 
-char ** words;
-sr_parseline(sr);   // parses current buffered line based on col sep
-		    // and parses ncols
+    sr_check_not_null( sr );
 
-/* destructors */
-sr_free(infile);    // cleanup
+    sr->fp = check_fopen( filename, "r" );
 
+    len = strlen( filename ) + 1;
+    sr->filename = check_alloc( len, sizeof( char ) );
+    strncpy( sr->filename, filename, len );
 
+    /* choose a reasonable default line buffer size */
+    sr_buf_alloc( sr, 1024 );
+    sr->line_num = 0;
+}
+
+void
+sr_clean( simple_reader * const sr )
+{
+    if( NULL != sr ) {
+        fclose( sr->fp );
+        CHECK_CLEAN( sr->filename );
+        sr_buf_clean( sr );
+        sr->line_num = 0;
+    }
+}
+
+simple_reader *
+sr_create( char const *const filename )
+{
+    simple_reader *sr;
+    sr = check_alloc( 1, sizeof( simple_reader ) );
+    sr_init( sr, filename );
+    return sr;
+}
+
+simple_reader *
+sr_destroy( simple_reader * sr )
+{
+    sr_clean( sr );
+    CHECK_CLEAN( sr );
+    return NULL;
+}
+
+static inline char *
+sr_line( simple_reader const *const sr )
+{
+    sr_check_not_null( sr );
+    return sr->buf;
+}
+
+static inline int
+sr_line_isempty( simple_reader const *const sr )
+{
+    sr_check_not_null( sr );
+
+    if( NULL == sr->buf )
+        return TRUE;
+    if( '\0' == sr->buf[0] )
+        return TRUE;
+
+    assert( sr->buf_len > 0 );
+    return FALSE;
+}
+
+static inline int
+sr_linenum( simple_reader const *const sr )
+{
+    sr_check_not_null( sr );
+    return sr->line_num;
+}
+
+static inline size_t
+sr_linelen( simple_reader const *const sr )
+{
+    sr_check_not_null( sr );
+    return sr->buf_len;
+}
+
+static inline char *
+sr_filename( simple_reader const *const sr )
+{
+    sr_check_not_null( sr );
+    return sr->filename;
+}
+
+int
+sr_eof( simple_reader const *const sr )
+{
+    sr_check_not_null( sr );
+    return feof( sr->fp );
+}
+
+char *
+sr_readline( simple_reader * const sr )
+{
+    char *res;
+    size_t l = 0;
+
+    sr_check_not_null( sr );
+
+    if( sr->buf_size < 2 ) {
+        fprintf( stderr, "Error: buffer too small! (max_length = %zd)\n", sr->buf_size - 2 );
+        exit( EXIT_FAILURE );
+    }
+
+    res = fgets( sr->buf, sr->buf_size, sr->fp );
+    if( NULL == res ) {         /* check for error or EOF */
+        if( feof( sr->fp ) )
+            return NULL;
+
+        fprintf( stderr, "Error: Cannot read beyond line %zd of file: %s\n",
+                 sr->line_num, sr->filename );
+
+        if( ferror( sr->fp ) )
+            perror( "Error:" );
+
+        exit( EXIT_FAILURE );
+    }
+
+    sr->line_num += 1;
+
+    l = strcspn( sr->buf, "\n" );
+    if( sr->buf_size <= l + 1 ) {
+        fprintf( stderr, "Error: exceeded maximum length (%zd) for line %zd in file: %s\n",
+                 sr_buf_maxlen( sr ), sr->line_num, sr->filename );
+        exit( EXIT_FAILURE );
+    }
+    sr->buf[l] = '\0';
+    sr->buf_len = l;
+
+    return sr->buf;
+}
+
+#endif
